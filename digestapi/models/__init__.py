4310388ef4d3f2a693578d0d72b96846d6fa459d
Gary Cason <garrettcason@skiff.com>
2024-06-17 14:48:03 -0500

    Set up book,category, and bookcategory tables with migrations

---
+++ digestapi/models/__init__.py
@@ -0,0 +1,3 @@
+from .book import Book
+from .category import Category
+from .bookcategory import BookCategory
---
+++ digestapi/models/bookcategory.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class BookCategory(models.Model):
+    book = models.ForeignKey('Book', on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
